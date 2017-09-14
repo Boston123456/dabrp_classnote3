38b1437af9182fb4be07ad370ce8665b28f0a1b9
June <stdpjh@naver.com>
2017-09-14 22:21:43 +0900

    terminal edit for root

---
+++ terminal edit root branch.R
@@ -0,0 +1,161 @@
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git remote
+$ git remote -v
+Root    https://github.com/mrchypark/dabrp_classnote3 (fetch)
+Root    https://github.com/mrchypark/dabrp_classnote3 (push)
+origin  https://github.com/Boston123456/dabrp_classnote3 (fetch)
+origin  https://github.com/Boston123456/dabrp_classnote3 (push)
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git pull
+Already up-to-date.
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ goit
+$ goi
+$ go
+$ git
+$ git remote
+$ git remote add
+$ git remote add root
+$ git remote add roo
+$ git remote add ro
+$ git remote add r
+$ git remote add
+$ git remote ad
+$ git remote a
+$ git remote
+$ git remot
+$ git remo
+$ git rem
+$ git re
+$ git r
+$ git
+$ gi
+$ g
+$
+$ git
+$ git remote
+$ git remote -v
+Root    https://github.com/mrchypark/dabrp_classnote3 (fetch)
+Root    https://github.com/mrchypark/dabrp_classnote3 (push)
+origin  https://github.com/Boston123456/dabrp_classnote3 (fetch)
+origin  https://github.com/Boston123456/dabrp_classnote3 (push)
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git remote
+$ git remote add
+$ git remote add root
+$ git remote add root https://github.com/mar
+$ git remote add root https://github.com/ma
+$ git remote add root https://github.com/mrchy/dabrp_classnote3
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git remote
+$ git remote -v
+Root    https://github.com/mrchypark/dabrp_classnote3 (fetch)
+Root    https://github.com/mrchypark/dabrp_classnote3 (push)
+origin  https://github.com/Boston123456/dabrp_classnote3 (fetch)
+origin  https://github.com/Boston123456/dabrp_classnote3 (push)
+root    https://github.com/mrchy/dabrp_classnote3 (fetch)
+root    https://github.com/mrchy/dabrp_classnote3 (push)
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git pull
+$ git pull origin
+$ git pull origin master
+From https://github.com/Boston123456/dabrp_classnote3
+ * branch            master     -> FETCH_HEAD
+Already up-to-date.
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git puul
+$ git puu
+$ git pull
+$ git pull root
+$ git pull root mster
+$ git pull root mste
+$ git pull root mst
+$ git pull root ms
+$ git pull root master
+remote: Repository not found.
+fatal: repository 'https://github.com/mrchy/dabrp_classnote3/' not found
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git remote
+$ git remote rm
+$ git remote rm root
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git remote
+$ git remote add
+$ git remote add root
+$ git remote add root http://
+$ git remote add root http:/
+$ git remote add root http:
+$ git remote add root http
+$ git remote add root htts
+$ git remote add root https:..
+$ git remote add root https:.
+$ git remote add root https://github.com/^V
+$ git remote add root https://github.com/
+$ git remote add root https://github.com.
+$ git remote add root https://github.com/mrchypark/dap
+$ git remote add root https://github.com/mrchypark/dabrp_classnote2
+$ git remote add root https://github.com/mrchypark/dabrp_classnote3
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git puu
+$ git pull
+$ git pull root
+$ git pull root master
+remote: Counting objects: 32, done.
+remote: Compressing objects: 100% (18/18), done.
+remote: Total 32 (delta 12), reused 27 (delta 7), pack-reused 0
+Unpacking objects:   9% (3/32)Unpacking objects:  31% (10/32)Unpacking objects:  37% (12/32)
+Unpacking objects:  53% (17/32)Unpacking objects:  59% (19/32)Unpacking objects:  68% (22/32)Unpacking objects:  78% (25/32)Unpacking objects:  90% (29/32)Unpacking objects: 100% (32/32), done.
+From https://github.com/mrchypark/dabrp_classnote3
+ * branch            master     -> FETCH_HEAD
+ * [new branch]      master     -> root/master
+error: Your local changes to the following files would be overwritten by merge:
+        nice.R
+Please commit your changes or stash them before you merge.
+Aborting
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+$ git
+$ git push
+Everything up-to-date
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
+
+jhpyo@SEOW237  ~/Documents/dabrp_classnote3 (remotegen)
+$
